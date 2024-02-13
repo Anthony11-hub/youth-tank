cfb11c1257dd90531d79c44a831cb7a5c9fe78ae
Leroy Mogire <ntruderleroy@gmail.com>
2024-02-13 22:14:47 +0300

    Member details update

--- assets/js/member-detail.js
+++ assets/js/member-detail.js
@@ -8,22 +8,24 @@ const memberName = urlParams.get("member");
  * @returns {Promise<any>} A Promise that resolves to the parsed JSON data
  */
 fetch("/json/members.json")
-  .then((response) => response.json())
-  .then((members) => {
-    const selectedMember = members.find((member) => member.name === memberName);
-    if (selectedMember) {
-      document.querySelector(".sec-title .name").textContent =
-        selectedMember.name;
-      document.querySelector(".hero-title .span").textContent =
-        selectedMember.name;
-      document.querySelector(".image-column .image img").src =
-        selectedMember.image;
-      document.querySelector(".inner-column .text").textContent =
-        selectedMember.bio;
-    } else {
-      console.error("Member not found");
-    }
-  })
-  .catch((error) => {
-    console.error("Error fetching data:", error);
-  });
+    .then((response) => response.json())
+    .then((members) => {
+        const selectedMember = members.find(
+            (member) => member.name === memberName
+        );
+        if (selectedMember) {
+            document.querySelector(".sec-title .name").textContent =
+                selectedMember.name;
+            document.querySelector(".hero-title .span").textContent =
+                selectedMember.name;
+            document.querySelector(".image-column .image img").src =
+                selectedMember.image;
+            document.querySelector(".inner-column .text").textContent =
+                selectedMember.bio;
+        } else {
+            console.error("Member not found");
+        }
+    })
+    .catch((error) => {
+        console.error("Error fetching data:", error);
+    });
